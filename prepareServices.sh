772d318bdcd705cc25224a5a9e81f681f88c9744
Geoffrey Aldebert <geoffrey.aldebert@data.gouv.fr>
2022-01-17 09:22:07 +0100

    Update init script

--- prepareServices.sh
+++ prepareServices.sh
@@ -1,6 +1,6 @@
-mkdir -p dags-airflow
 mkdir -p pg-airflow
 mkdir -p plugins-airflow
 cp .envExample .env
-echo "AIRFLOW_UID=$(id -u)\nAIRFLOW_GID=0" >> .env
-
+echo "AIRFLOW_UID=$(id -u)" >> .env
+echo "AIRFLOW_GID=0" >> .env
+echo "Done"
